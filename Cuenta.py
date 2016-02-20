a788ec551750e2372e6f72284d53ecd46bd16fb9
Jose A <jvazquez@bnl.gov>
2016-02-20 00:32:43 -0500

    added latex file

--- Cuenta.py
+++ Cuenta.py
@@ -9,9 +9,11 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
     #Information about people
+Make_plot = False
+Edo_cuenta= True
 month     = ['Feb']
 investors = ['Alan', 'Sandra']
-directory = '/Users/josevazquezgonzalez/Desktop/TODOs/Investing/New_test/Ahorro'
+directory = '/Users/josevazquezgonzalez/Desktop/TODOs/Investing/Ahorro'
 
 today = datetime.date.today()
 hoy   = int(today.strftime('%d'))
@@ -19,10 +21,7 @@ hoy   = int(today.strftime('%d'))
 
     #Select a particular person
 if len(sys.argv) > 1 :
-    if sys.argv[1] in investors:
-        Person = eval(sys.argv[1])()
-    else:
-        sys.exit("** Error: Person not found in the list")
+    Person = eval(sys.argv[1])()
 else:
     sys.exit("** Error: Write person's name")
 
@@ -40,21 +39,26 @@ sum_tot_depos, final = A.total()
 
     #Now we produce some plots
 
-fig = plt.figure(figsize = (12, 7))
-gs  = gridspec.GridSpec(1, 2, width_ratios= [3, 2.5])
+if  Make_plot:
+    fig = plt.figure(figsize = (12, 7))
+    gs  = gridspec.GridSpec(1, 2, width_ratios= [3, 2.5])
 
-make_plot(plt.subplot(gs[0]), dates, depos,
+    make_plot(plt.subplot(gs[0]), dates, depos,
           title='Depositos realizados',
           legend='Depositado = $%5.2f'%(sum_tot_depos[-1]),
           ylabel='Depositos (MXN)')
 
-make_plot(plt.subplot(gs[1]), A.months, final, color='Magenta',
+    make_plot(plt.subplot(gs[1]), A.months, final, color='Magenta',
           title='Total acumulado',
           legend='Total = $%5.2f'%(final[-1]),
           ylabel='Total (MXN)',
           extra_yinfo= sum_tot_depos, extra_color='DodgerBlue')
 
-plt.subplots_adjust(wspace=0.4)
-plt.show()
+    plt.subplots_adjust(wspace=0.4)
+    plt.show()
+
+if Edo_cuenta:
+    make_latex(Person, month)
+
 
 #Lates is next
--- Useful.py
+++ Useful.py
@@ -39,3 +39,66 @@ def make_plot(ax, xinfo, yinfo, color='lime', legend="legend",
 
     pylab.ylim([0, np.array(yinfo).max()*1.2])
     ax.grid()
+
+
+
+def make_latex(Person, month):
+    with open('Edo_%s_%s.tex'%(Person.name, month[0]), 'w') as f:
+
+        input_1 = """
+\documentclass[11pt,secnumarabic,nofootinbib,preprintnumbers,amsmath,amssymb,aps]{revtex4}
+\usepackage[usenames]{color}
+\usepackage{graphicx}
+\usepackage{ulem}
+\usepackage{hyperref}
+
+\\frenchspacing
+\def\\red{\\textcolor{red}}
+\def\\blue{\\textcolor{blue}}
+\\begin{document}
+        """
+        f.write(input_1 + '\n')
+
+        f.write('\\title{ESTADO DE CUENTA: %s.2016}\n'%(month[0]))
+        f.write('\\author{%s}\n'%(Person.full_name))
+        f.write('\\email[Contacto:~]{%s}\n'%(Person.email))
+
+
+        input_2 = """
+\date{\\today}\n
+\maketitle\n
+
+Estado de cuenta correspondiente al mes de %s del 2016, considerando una
+tasa de inter\\'es del %.1f\%% anual~\\footnote{IVA incluido.}.\n
+        """%(month[0], Person.perct*100*12)
+        f.write(input_2 + '\n\n')
+
+
+        input_3 = """
+\\begin{table}[h!]
+\\begin {center}
+\\begin{tabular}{rccl}
+\hline
+\hline
+\\vspace{0.1cm}
+Fecha de transacci\\'on  \qquad \qquad & Monto \qquad \qquad&  \qquad \qquad Inter\\'es & \qquad  \qquad Total  \\\\
+\hline
+\\vspace{0.1cm}
+        """
+        f.write(input_3 + '\n\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
