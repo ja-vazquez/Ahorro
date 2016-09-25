9849d869e342721d567b1cf47a35458dcd90748c
Jose A <jvazquez@bnl.gov>
2016-09-24 22:24:43 -0400

    gathering files

--- my_account.py
+++ my_account.py
@@ -13,9 +13,11 @@ inv_names = df['names'].tolist()
 today_date  = datetime.date.today()
 today_day   = str(today_date.strftime('%d'))
 
-#bursa = 93522.7
-bursa =float( input("Bursanet total: "))
+dir = '/Users/josevazquezgonzalez/Desktop/TODOs/Finances/Investing/Bursanet/'
+pd_bursa = pd.read_csv(dir + 'Investing.txt', skiprows=4, sep='\s+',
+                       names=['dates', 'fees', 'money_in', 'total'])
 
+bursa = pd_bursa['total'].iloc[-1]
 
 for inv in inv_names:
     direc = 'Investors/{}/{}/'.format(inv, month)
@@ -26,6 +28,7 @@ for inv in inv_names:
 result = pd.concat(dt)
 result['names'] = inv_names
 result.index =  result['names']
+
 
 ser = result[['cum_total']].sum()
 ser.set_value('bursa', bursa)
@@ -43,13 +46,17 @@ if True:
 
  ax1= plt.subplot(gs[0])
  result[['cum_depos', 'cum_total']].plot(kind = 'bar', ax=ax1)
- plt.title('Available: %.2f'%(avail))
+ plt.title('Yo: %.1f , Investors: %.1f'%(avail, result[['cum_total']].iloc[0:-1].sum()))
  #print (result)
 
  ax2= plt.subplot(gs[1])
- ser.plot(kind = 'bar', ax=ax2, label = 'Bursa: %.2f \n Total: %.2f'%(ser['bursa'], ser['cum_total']))
+ ser['investors']= ser['bursa']-ser['cum_total']
+ ser['interests']= result.ix['Yo']['cum_interest']
+ ser[['investors', 'interests']].plot(kind = 'bar', ax=ax2, label = 'Bursa: %.1f \n Yo:            %.1f :  %.2f %% \n Investors: %.1f :   %.2f %%'
+                            %(ser['bursa'], result.ix['Yo']['cum_interest'], result.ix['Yo']['cum_interest']/result.ix['Yo']['cum_total']*100,
+                              ser['bursa']- ser['cum_total'], (ser['bursa']- ser['cum_total'])/result[['cum_total']].iloc[0:-1].sum()*100.))
  plt.legend(loc='lower center')
- plt.title('Total = %.2f'%(ser['bursa'] - ser['cum_total']))
+ plt.title('Total = %.1f'%(avail - result.ix['Yo']['cum_depos']))
  plt.savefig('Investors/Yo/'+month+'/Final_Yo_' + month + '_' + today_day + '.pdf')
  #plt.show()
  
--- run_all.py
+++ run_all.py
@@ -5,8 +5,13 @@ import pandas as pd
 df = pd.read_csv('inv_names.csv',names=['names'])
 inv_names = df['names'].tolist()
 
+
 for inv in inv_names:
 	os.system(r"python2.7 Cuenta.py %s"%(inv))
 
-os.system("python2.7 my_account.py")
-
+commd = """
+python2.7 my_account.py
+cd ../Investing/
+python2.7 investing.py
+python2.7 investing.py e"""
+os.system(commd)
