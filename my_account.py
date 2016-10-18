fc0e6d6f3a10f5fdc65211bea195eb53d41d2c0a
Jose A <jvazquez@bnl.gov>
2016-10-17 23:38:20 -0400

    double checking

--- my_account.py
+++ my_account.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pylab as plt
 from matplotlib import gridspec
 
-month = 'Sep'
+month = 'Oct'
 dt =[]
 df = pd.read_csv('inv_names.csv',names=['names'])
 inv_names = df['names'].tolist()
@@ -32,6 +32,7 @@ result.index =  result['names']
 
 ser = result[['cum_total']].sum()
 ser.set_value('bursa', bursa)
+print(ser)
 avail = ser['bursa'] - result[['cum_total']].iloc[0:-1].sum()
 
 commd = """ mkdir Investors/Yo/%s"""%(month)
@@ -46,17 +47,17 @@ if True:
 
  ax1= plt.subplot(gs[0])
  result[['cum_depos', 'cum_total']].plot(kind = 'bar', ax=ax1)
- plt.title('Yo: %.1f , Investors: %.1f'%(avail, result[['cum_total']].iloc[0:-1].sum()))
+ plt.title('Investors: %.1f,  Avail: %.1f'%(result[['cum_total']].sum(), avail))
  #print (result)
 
  ax2= plt.subplot(gs[1])
  ser['investors']= ser['bursa']-ser['cum_total']
  ser['interests']= result.ix['Yo']['cum_interest']
  ser[['investors', 'interests']].plot(kind = 'bar', ax=ax2, label = 'Bursa: %.1f \n Yo:            %.1f :  %.2f %% \n Investors: %.1f :   %.2f %%'
-                            %(ser['bursa'], result.ix['Yo']['cum_interest'], result.ix['Yo']['cum_interest']/result.ix['Yo']['cum_total']*100,
-                              ser['bursa']- ser['cum_total'], (ser['bursa']- ser['cum_total'])/result[['cum_total']].iloc[0:-1].sum()*100.))
- plt.legend(loc='lower center')
- plt.title('Total = %.1f'%(avail - result.ix['Yo']['cum_depos']))
+                            %(ser['bursa'], result.ix['Yo']['cum_interest'], result.ix['Yo']['cum_interest']/result.ix['Yo']['cum_depos']*100,
+		              ser['bursa']- ser['cum_total'], (ser['bursa'] - ser['cum_total'])/result[['cum_depos']].sum()*100)) 
+ plt.legend(loc='best')
+ plt.title('Total = %.1f'%(ser['bursa']- ser['cum_total'] + result.ix['Yo']['cum_interest']))
  plt.savefig('Investors/Yo/'+month+'/Final_Yo_' + month + '_' + today_day + '.pdf')
  #plt.show()
  
